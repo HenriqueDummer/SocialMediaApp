0bf6aebf640497cb5299ba852966095532d3ec2d
Henrique Dummer <hatiehique2005@gmail.com>
2025-01-30 10:38:43 -0300

    Basic routing

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import SigninForm from './../_auth/forms/SigninForm';
+import SignupForm from './../_auth/forms/SignupForm';
+import AuthLayout from './../_auth/AuthLayout';
+import Home from './../_root/pages/Home';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />}/>
+          <Route path="/sign-up" element={<SignupForm />}/>
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
