6053d42ad15d1aa26234aafb8785d5d7367d5742
HenriqueDummer <hatiehique2005@gmail.com>
2025-05-26 13:34:46 -0300

    add origin on cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://192.168.210.110:5173", "https://socialmediaapp-1fop.onrender.com"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://192.168.210.110:5173", "https://vibe-link-three.vercel.app"],
   credentials: true
 }));
 
